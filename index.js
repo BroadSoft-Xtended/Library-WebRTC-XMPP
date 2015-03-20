570eea6bfb6b7cceec905a4520a5f685b671fe7e
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-03-20 03:50:18 -0600

    fix index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = {view: require('./lib/views/authentication'), model: require('./lib/models/authentication')};
+module.exports = {view: require('./lib/views/xmpp'), model: require('./lib/models/xmpp')};
