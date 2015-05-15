0b8c71486ac176ddf31fe24a46c8da5040307185
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-05-15 08:32:33 -0600

    remove popupview and integrate &yet xmpp

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,1 @@
-module.exports = ".bdsft-client .enableXMPP.xmpp-shown .xmpp{transition:all 1s linear;opacity:1;z-index:100}.bdsft-client .enableXMPP.xmpp-hidden .xmpp{transition:all 1s linear;opacity:0;z-index:-1}"
+module.exports = ".bdsft-client .xmpp{width:400px;height:200px}.bdsft-client .enableXMPP.xmpp-shown.xmpp{transition:all 1s linear;opacity:1;z-index:100}.bdsft-client .enableXMPP.xmpp-hidden.xmpp{transition:all 1s linear;opacity:0;z-index:-1}"
