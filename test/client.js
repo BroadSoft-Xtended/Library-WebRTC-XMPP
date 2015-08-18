322494174333d34d3a8fb654709a11f48331bb62
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-08-18 09:38:18 -0600

    WRTC-23 : start changing stanza with node-xmpp-client

---
+++ test/client.js
@@ -0,0 +1,31 @@
+var test = require('../node_modules/webrtc-core/test/includes/common');
+var extend = require('extend');
+var chai = require("chai");
+chai.use(require("chai-as-promised"));
+var should = chai.should();
+
+describe('client', function() {
+
+  before(function() {
+    test.createModelAndView('xmpp', {
+        xmpp: require('../'),
+        sound: require('webrtc-sound'),
+        dms: require('webrtc-dms')
+    });
+    config = require('./config/default.json');
+    try {
+      extend(config, require('./config/test.json'));
+    } catch(e) {}
+    require('debug').enable('xmpp*');
+    client =  bdsft_client_instances.test.xmpp.client;
+  });
+
+  it('connect and disconnect', function() {
+    return client.connect({jid: config.user, password: config.xmppPassword}).then(function(){
+      return client.disconnect().should.eventually.be.fulfilled;
+    });
+  });
+  it('connect with wrong password', function() {
+    return client.connect({jid: config.user, password: 'wrongpassword'}).should.eventually.be.rejected;
+  });
+});
